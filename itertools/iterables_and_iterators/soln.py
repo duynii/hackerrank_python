2c3bc161cafeff7676e68f42a58971931182a2c5
duynii <duynii@gmail.com>
2017-06-09 18:47:18 +1000

    itertools/iterables_and_iterators: added, version 2

--- itertools/iterables_and_iterators/soln.py
+++ itertools/iterables_and_iterators/soln.py
@@ -11,16 +11,23 @@ indices = list( range(n) )
 comb = list( combinations(indices, k) )
 #print( comb )
 
-has_a = set()
-for i in range(n):
-    if( ll[i] == 'a' ):
-        has_a.add(i)
+# has_a = set()
+# for i in range(n):
+#     if( ll[i] == 'a' ):
+#         has_a.add(i)
         
+has_a = set( filter( lambda i : ll[i] == 'a' , range(n) ) )
+#print( has_a )       
+    
 #print(has_a)
 
-count = 0
-for a in comb:
-    if has_a & set(a):
-        count += 1
+# count = 0
+# for a in comb:
+#     if has_a & set(a):
+#         count += 1
         
-print(  count/len(comb) )
+# print(  count/len(comb) )
+        
+a_list = list( filter( lambda x : set(x) & has_a, comb) )
+
+print( len(a_list) / len(comb) )
