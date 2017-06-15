ee73083a25572c605359ceb1e80df4405db62091
dewey <dewey@acfr.usyd.edu.au>
2017-06-15 14:37:44 +1000

    time_delta.py: working version using strptime

--- date_and_time/time_delta.py
+++ date_and_time/time_delta.py
@@ -1,28 +1,6 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-n = int(input())
+from datetime import datetime as dt
 
-
-import calendar
-import datetime
-
-months = list(calendar.month_abbr)
-
-def get_datetime():
-    date, day, month, year, time, tz = input().split()
-    day = int(day)
-    year = int(year)
-    month = months.index(month)
-    hour, mins, secs = map(int, time.split(':'))
-    dt = datetime.datetime(year, month, day, hour, mins, secs)
-    
-    mins_tz = int(tz) % 100
-    hours_tz = int( int(tz) / 100 )
-    dt -= datetime.timedelta(  minutes=( mins_tz + hours_tz*60 ) )
-    #print( dt )
-    return dt
-    
-def print_delta():
-    print( int((get_datetime() - get_datetime()).total_seconds()) ) 
-    
-for _ in range(n):
-    print_delta()
+fmt = '%a %d %b %Y %H:%M:%S %z'
+for i in range(int(input())):
+    print(int(abs((dt.strptime(input(), fmt) - 
+                   dt.strptime(input(), fmt)).total_seconds())))
