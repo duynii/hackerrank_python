0401436881025c793dace78a85bf0e18c246904e
duynii <duynii@gmail.com>
2017-06-17 15:05:58 +1000

    builtins/zipped.py: shows alternative solns

--- builtins/zipped.py
+++ builtins/zipped.py
@@ -1,4 +1,6 @@
 n, x = map(int, input().split())
+# Using list comprehension, same speed
+# n, x = [int(x) for x in input().split()]
 
 subjects = []
 for _ in range(x):
@@ -6,3 +8,6 @@ for _ in range(x):
     
 for tup in zip(*subjects):
     print( sum(tup) / len(tup) )
+
+# Alternative one liner
+# [print(sum(i) / len(i) ) for i in zip( *[map(float, input().split()) for _ in range(int(input().split()[1])) ] ) ]
